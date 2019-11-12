df103eeaf01d79a018255e275aad795d2a153b48
Piotr Błaszyński <piotr@localhost.localdomain>
2019-11-12 01:51:40 +0100

    Dynamic memory allocation

--- tclo.c
+++ tclo.c
@@ -1,47 +1,62 @@
 #include<stdio.h> 
+#include<stdlib.h> 
   
-#define V 4 
-void printMatrix(int matrix[][V]); 
-
-void computeTC(int graph[][V]) 
+void printMatrix(int **, int); 
+int **allocateMatrix(int);
+void computeTC(int **matrix, int N) 
 { 
-    int reach[V][V], i, j, k; 
-    for (i = 0; i < V; i++) 
-        for (j = 0; j < V; j++) 
-            reach[i][j] = graph[i][j]; 
+    int **reach = allocateMatrix(N);
+    int i, j, k; 
+    for (i = 0; i < N; i++) 
+        for (j = 0; j < N; j++) 
+            reach[i][j] = matrix[i][j]; 
 #pragma scop  
-    for (k = 0; k < V; k++) 
+    for (k = 0; k < N; k++) 
     { 
-        for (i = 0; i < V; i++) 
+        for (i = 0; i < N; i++) 
         { 
-            for (j = 0; j < V; j++) 
+            for (j = 0; j < N; j++) 
             { 
                 reach[i][j] = reach[i][j] || (reach[i][k] && reach[k][j]); 
             } 
         } 
     } 
 #pragma endscop
-    printMatrix(reach); 
+    printMatrix(reach, N); 
 } 
 
-void printMatrix(int matrix[][V])
+void printMatrix(int **matrix, int N)
 {
-    for (int i = 0; i < V; i++)
+    for (int i = 0; i < N; i++)
     {
-        for (int j = 0; j < V; j++)
+        for (int j = 0; j < N; j++)
             printf ("%d ", matrix[i][j]);
         printf("\n");
     }
 }
-
+int **allocateMatrix(int N)
+{
+	int **t = (int **) malloc(sizeof(int *) * N);
+	for (int i = 0 ; i < N ; i++)
+	{
+		t[i] = (int *) malloc(sizeof(int) * N);
+	}
+	return t;
+}
 int main(void)
 {
-    int graph[V][V] = { {1, 1, 0, 1},
+	const int N = 4;
+	int **graph = allocateMatrix(N);
+	int g[4][4]=	{ {1, 1, 0, 1},
                         {0, 1, 1, 0},
                         {0, 0, 1, 1},
                         {0, 0, 0, 1}
                       };
-    printMatrix(graph);
-    computeTC(graph);
+    for (int i = 0; i < N; i++)
+        for (int j = 0; j < N; j++)
+            graph[i][j] = g [i][j];
+	
+    printMatrix(graph, 4);
+    computeTC(graph, 4);
     return 0;
 }
--- tclo.pluto.c
+++ tclo.pluto.c
@@ -6,28 +6,29 @@
 #define min(x,y)    ((x) < (y)? (x) : (y))
 
 #include<stdio.h> 
+#include<stdlib.h> 
   
-#define V 4 
-void printMatrix(int matrix[][V]); 
-
-void computeTC(int graph[][V]) 
+void printMatrix(int **, int); 
+int **allocateMatrix(int);
+void computeTC(int **matrix, int N) 
 { 
-    int reach[V][V], i, j, k; 
-    for (i = 0; i < V; i++) 
-        for (j = 0; j < V; j++) 
-            reach[i][j] = graph[i][j]; 
+    int **reach = allocateMatrix(N);
+    int i, j, k; 
+    for (i = 0; i < N; i++) 
+        for (j = 0; j < N; j++) 
+            reach[i][j] = matrix[i][j]; 
   int t1, t2, t3, t4, t5;
  int lb, ub, lbp, ubp, lb2, ub2;
  register int lbv, ubv;
-if (V >= 1) {
-  for (t1=0;t1<=V-1;t1++) {
-    for (t2=0;t2<=floord(V-1,16);t2++) {
-      lbp=max(0,ceild(32*t2-V+1,32));
-      ubp=min(floord(V-1,32),t2);
+if (N >= 1) {
+  for (t1=0;t1<=N-1;t1++) {
+    for (t2=0;t2<=floord(N-1,16);t2++) {
+      lbp=max(0,ceild(32*t2-N+1,32));
+      ubp=min(floord(N-1,32),t2);
 #pragma omp parallel for private(lbv,ubv,t4,t5)
       for (t3=lbp;t3<=ubp;t3++) {
-        for (t4=32*t2-32*t3;t4<=min(V-1,32*t2-32*t3+31);t4++) {
-          for (t5=32*t3;t5<=min(V-1,32*t3+31);t5++) {
+        for (t4=32*t2-32*t3;t4<=min(N-1,32*t2-32*t3+31);t4++) {
+          for (t5=32*t3;t5<=min(N-1,32*t3+31);t5++) {
             reach[t4][t5] = reach[t4][t5] || (reach[t4][t1] && reach[t1][t5]);;
           }
         }
@@ -35,27 +36,41 @@ if (V >= 1) {
     }
   }
 }
-    printMatrix(reach); 
+    printMatrix(reach, N); 
 } 
 
-void printMatrix(int matrix[][V])
+void printMatrix(int **matrix, int N)
 {
-    for (int i = 0; i < V; i++)
+    for (int i = 0; i < N; i++)
     {
-        for (int j = 0; j < V; j++)
+        for (int j = 0; j < N; j++)
             printf ("%d ", matrix[i][j]);
         printf("\n");
     }
 }
-
+int **allocateMatrix(int N)
+{
+	int **t = (int **) malloc(sizeof(int *) * N);
+	for (int i = 0 ; i < N ; i++)
+	{
+		t[i] = (int *) malloc(sizeof(int) * N);
+	}
+	return t;
+}
 int main(void)
 {
-    int graph[V][V] = { {1, 1, 0, 1},
+	const int N = 4;
+	int **graph = allocateMatrix(N);
+	int g[4][4]=	{ {1, 1, 0, 1},
                         {0, 1, 1, 0},
                         {0, 0, 1, 1},
                         {0, 0, 0, 1}
                       };
-    printMatrix(graph);
-    computeTC(graph);
+    for (int i = 0; i < N; i++)
+        for (int j = 0; j < N; j++)
+            graph[i][j] = g [i][j];
+	
+    printMatrix(graph, 4);
+    computeTC(graph, 4);
     return 0;
 }
