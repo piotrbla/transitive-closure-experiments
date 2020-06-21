f36892878697cadff13a91905947fe8269b1da68
Piotr B <piotrb@gmail.com>
2020-06-21 17:45:30 +0200

    New perfect loops

--- nus/NusValidation.cpp
+++ nus/NusValidation.cpp
@@ -120,34 +120,33 @@ void computeDYN3PerfectB(int** table, int n, int *seq) {
 //czwarty element jest to "k"
 //piąty element jeśli jest "0" oznacza "s1", 
 //natomiast jeśli "1" oznacza, ze maja byc  2 instrukcje wstawione - s1 i s2  
-//S[i][j] = max_score(S[i][k] + S[k+1][j], S[i][j]); // s1
-//S[i][j] = max_score(S[i][j], S[i+1][j-1] + sigma(i, j)); // s2
 #pragma scop
-    for (int c0 = 1; c0 < n; c0 += 1)
-    {
-        for (int c1 = 0; c1 < n - c0; c1 += 1)
-        {
-            if (c0 >= 2)
-            {
-                S[c1][c0 + c1] = max_score(S[c1][c1] + S[c1+1][c0 + c1], S[c1][c0 + c1]); // s1
-                S[c1][c0 + c1] = max_score(S[c1][c0 + c1], S[c1+1][c0 + c1-1] + sigma(c1, c0 + c1)); // s2
-                //(c0, c1, c0 + c1, c1, 1);
-            }
-            S[c1][c0 + c1] = max_score(S[c1][c0 + c1 - 1] + S[c0 + c1 - 1 + 1][c0 + c1], S[c1][c0 + c1]); // s1
-            S[c1][c0 + c1] = max_score(S[c1][c0 + c1], S[c1+1][c0 + c1 - 1] + sigma(c1, c0 + c1)); // s2
-            //(c0, c1, c0 + c1, c0 + c1 - 1, 1);
-            for (int c2 = c0 + c1 + 1; c2 < min(n, 2 * c0 + c1); c2 += 1)
-            {
-                if (2 * c0 + c1 >= c2 + 2)
-                {
-                  S[c1][c2] = max_score(S[c1][c0 + c1 - 1] + S[c0 + c1 - 1 + 1][c2], S[c1][c2]); // s1
+  for (int c0 = 1; c0 < n; c0 += 1)
+  {
+      for (int c1 = 0; c1 < n - c0; c1 += 1)
+      {
+          for (int c2 = c0 + c1; c2 < min(n, 2 * c0 + c1); c2 += 1)
+          {
+              if (2 * c0 + c1 >= c2 + 2)
+              {
+                  S[c1][c2] = max_score(S[c1][-c0 + c2] + S[-c0 + c2+1][c2], S[c1][c2]); // s1
                   //(c0, c1, c2, -c0 + c2, 0);
-                }
-                S[c1][c2] = max_score(S[c1][c0 + c1 - 1] + S[c0 + c1 - 1 + 1][c2], S[c1][c2]); // s1
-                //(c0, c1, c2, c0 + c1 - 1, 0);
-            }
-        }
-    }
+                  if (c2 == c0 + c1)
+                  {
+                      S[c1][c2] = max_score(S[c1][c2], S[c1+1][c2-1] + sigma(c1, c2)); // s2
+                      //(c0, c1, c0 + c1, c1, 1);
+                  }
+              }
+              S[c1][c2] = max_score(S[c1][c0 + c1 - 1] + S[c0 + c1 - 1 + 1][c2], S[c1][c2]); // s1
+              //(c0, c1, c2, c0 + c1 - 1, 0);
+              if (c2 == c0 + c1)
+              {
+                  S[c1][c2] = max_score(S[c1][c2], S[c1+1][c2-1] + sigma(c1, c2)); // s2
+                  //(c0, c1, c0 + c1, c0 + c1 - 1, 1);
+              }
+          }
+      }
+  }
 #pragma endscop
   double execution_time = omp_get_wtime() - start;
 
