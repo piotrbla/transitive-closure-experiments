25bef7fddcf9f51e9e1533060cbc3f8f62fed7b2
Piotr Błaszyński <piotrb@gmail.com>
2020-12-02 14:51:07 +0100

    Nussinov's testing added

---
+++ nus/runned/compile.sh
@@ -0,0 +1,1 @@
+gcc -std=c99 -DMYTHREADS=32 -o nusd_exe -O2 nusd.c -fopenmp -lm
---
+++ nus/runned/go_to_work.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+./compile.sh
+cp results.txt  old_results_"$(date +"%Y%m%d_%H%M%S")".csv
+rm results.txt
+./run.sh
