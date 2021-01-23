9100b3e7e5acb31efd49256efcccc0187b3f6094
Piotr Błaszyński <piotrb@gmail.com>
2021-01-23 17:23:29 +0100

    Files comparator, functions prototypes

--- livermorec/Livermorec.cpp
+++ livermorec/Livermorec.cpp
@@ -1,87 +1,22 @@
 #pragma warning(disable : 4996)
-#include <math.h>
+#include <fstream>
+#include <iostream>
 #include <omp.h>
 #include <stdio.h>
-#include <stdlib.h>
 #define _CRT_SECURE_NO_WARNINGS
 
-void write_results_full(int n, double execution_time, char end_char)
-{
-  FILE* f = fopen("results.txt", "at");
-  fprintf(f, "%d:%lf%c", n, execution_time, end_char);
-  fclose(f);
-}
+bool is_files_equal(const char * , const char * ) ;
+void write_results_full(int, double, char);
+void write_results(int, double );
+int** allocate_matrix(int) ;
 
-void write_results(int n, double execution_time)
-{
-  write_results_full(n, execution_time, ';');
-}
+int **get_full_copy(int ** , int);
+int* allocate_vector(int);
+int *get_vector_copy(int *, int);
 
-int** allocateMatrix(int N) {
-  int** t = (int**)malloc(sizeof(int*) * N);
-  for (int i = 0; i < N; i++) {
-    t[i] = (int*)malloc(sizeof(int) * N);
-  }
-  return t;
-}
-
-int **getFullCopy(int ** table, int N)
-{
-  int **S = allocateMatrix(N);
-  for (int i = 0; i < N; i++)
-    for (int j = 0; j < N; j++)
-      S[i][j] = table[i][j];
-  return S;
-}
-
-int* allocateVector(int N) {
-  int* t = (int*)malloc(sizeof(int) * N);
-  return t;
-}
-
-int *getVectorCopy(int *table, int N)
-{
-  int *S = allocateVector(N);
-  for (int i = 0; i < N; i++)
-      S[i] = table[i];
-  return S;
-}
-
-
-void deallocateMatrix(int **t, int N) {
-  for (int i = 0; i < N; i++) {
-    free(t[i]);
-  }
-  free(t);
-}
-
-void printVector(int* vector, int N) {
-  static int fileno=1;
-  char filename[12];
-  sprintf(filename, "resVec_%d", fileno);
-  FILE* f = fopen(filename, "wt");
-  for (int i = 0; i < N; i++) {
-    fprintf(f, "%d ", vector[i]);
-    fprintf(f, "\n");
-  }
-  fclose(f);
-  fileno++;
-}
-
-void printMatrix(int** matrix, int N) {
-  static int fileno=1;
-  char filename[10];
-  sprintf(filename, "resMat_%d", fileno);
-  FILE* f = fopen(filename, "wt");
-  for (int i = 0; i < N; i++) {
-    for (int j = 0; j < N; j++)
-      fprintf(f, "%d ", matrix[i][j]);
-    fprintf(f, "\n");
-  }
-  fclose(f);
-  fileno++;
-}
-
+void deallocate_matrix(int **, int ) ;
+void print_vector(int* , int );
+void print_matrix(int**, int);
 
 /*
   *******************************************************************
@@ -96,8 +31,8 @@ void printMatrix(int** matrix, int N) {
 
 void LMKernel6_01_Base(int loop, int n, int *input_w, int **input_b)
 {
-  int** b = getFullCopy(input_b, n);
-  int* w = getVectorCopy(input_w, n);
+  int** b = get_full_copy(input_b, n);
+  int* w = get_vector_copy(input_w, n);
 
   double start = omp_get_wtime();
 
@@ -114,17 +49,17 @@ void LMKernel6_01_Base(int loop, int n, int *input_w, int **input_b)
 
   printf("BAS_01: %lf\n", execution_time);
   write_results(n, execution_time);
-  printVector(w, n);
-  printMatrix(b, n);
-  deallocateMatrix(b, n);
+  print_vector(w, n);
+  print_matrix(b, n);
+  deallocate_matrix(b, n);
   free(w);
   return;
 }
 
 void LMKernel6_02_Modification(int loop, int n, int *input_w, int **input_b)
 {
-  int** b = getFullCopy(input_b, n);
-  int* w = getVectorCopy(input_w, n);
+  int** b = get_full_copy(input_b, n);
+  int* w = get_vector_copy(input_w, n);
 
   double start = omp_get_wtime();
 
@@ -137,12 +72,13 @@ void LMKernel6_02_Modification(int loop, int n, int *input_w, int **input_b)
       }
     }
   }
+  w[7]=33;
   double execution_time = omp_get_wtime() - start;
 
   printf("MOD_02: %lf\n", execution_time);
   write_results(n, execution_time);
-  printVector(w, n);
-  deallocateMatrix(b, n);
+  print_vector(w, n);
+  deallocate_matrix(b, n);
   free(w);
   return;
 }
@@ -160,8 +96,8 @@ int main()
   const int LMAX = 10;
 #endif
 
-  int** input_b = allocateMatrix(ZMAX);
-  int* result_w = allocateVector(ZMAX);
+  int** input_b = allocate_matrix(ZMAX);
+  int* result_w = allocate_vector(ZMAX);
   for (int i = 0; i < ZMAX; i++)
     for (int j = 0; j < ZMAX; j++)
       input_b[i][j] = 0;
@@ -182,8 +118,105 @@ int main()
 
   LMKernel6_01_Base(LMAX, ZMAX, result_w, input_b);
   LMKernel6_02_Modification(LMAX, ZMAX, result_w, input_b);
-  deallocateMatrix(input_b, ZMAX);
+  deallocate_matrix(input_b, ZMAX);
   free(result_w);
+
+  if (is_files_equal("resVec_1", "resVec_2"))
+    std::cout << "OK\n";
+  else
+    std::cout << "ERROR\n";
+
 
 }
 
+bool is_files_equal(const char * filename1, const char * filename2) {
+  std::ifstream f1(filename1, std::ifstream::binary | std::ifstream::ate);
+  std::ifstream f2(filename2, std::ifstream::binary | std::ifstream::ate);
+  if (f1.fail() || f2.fail()) {
+    return false;
+  }
+  if (f1.tellg() != f2.tellg()) {
+    return false;
+  }
+  f1.seekg(0, std::ifstream::beg);
+  f2.seekg(0, std::ifstream::beg);
+  return std::equal(std::istreambuf_iterator <char >(f1.rdbuf()),
+    std::istreambuf_iterator <char >(),
+    std::istreambuf_iterator <char >(f2.rdbuf()));
+}
+void write_results_full(int n, double execution_time, char end_char)
+{
+  FILE* f = fopen("results.txt", "at");
+  fprintf(f, "%d:%lf%c", n, execution_time, end_char);
+  fclose(f);
+}
+
+void write_results(int n, double execution_time)
+{
+  write_results_full(n, execution_time, ';');
+}
+
+int** allocate_matrix(int N) {
+  int** t = (int**)malloc(sizeof(int*) * N);
+  for (int i = 0; i < N; i++) {
+    t[i] = (int*)malloc(sizeof(int) * N);
+  }
+  return t;
+}
+
+int **get_full_copy(int ** table, int N)
+{
+  int **S = allocate_matrix(N);
+  for (int i = 0; i < N; i++)
+    for (int j = 0; j < N; j++)
+      S[i][j] = table[i][j];
+  return S;
+}
+
+int* allocate_vector(int N) {
+  int* t = (int*)malloc(sizeof(int) * N);
+  return t;
+}
+
+int *get_vector_copy(int *table, int N)
+{
+  int *S = allocate_vector(N);
+  for (int i = 0; i < N; i++)
+      S[i] = table[i];
+  return S;
+}
+
+
+void deallocate_matrix(int **t, int N) {
+  for (int i = 0; i < N; i++) {
+    free(t[i]);
+  }
+  free(t);
+}
+
+void print_vector(int* vector, int N) {
+  static int fileno=1;
+  char filename[12];
+  sprintf(filename, "resVec_%d", fileno);
+  FILE* f = fopen(filename, "wt");
+  for (int i = 0; i < N; i++) {
+    fprintf(f, "%d ", vector[i]);
+    fprintf(f, "\n");
+  }
+  fclose(f);
+  fileno++;
+}
+
+void print_matrix(int** matrix, int N) {
+  static int fileno=1;
+  char filename[10];
+  sprintf(filename, "resMat_%d", fileno);
+  FILE* f = fopen(filename, "wt");
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++)
+      fprintf(f, "%d ", matrix[i][j]);
+    fprintf(f, "\n");
+  }
+  fclose(f);
+  fileno++;
+}
